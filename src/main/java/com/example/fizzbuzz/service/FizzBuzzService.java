8d4d14551249f7d6cbcbf6aab53d9414bb7e52d7
Anarane Tung <66110957+anaranetung@users.noreply.github.com>
2025-03-22 20:19:09 +0800

    fix failing test case - when n is 5

--- src/main/java/com/example/fizzbuzz/service/FizzBuzzService.java
+++ src/main/java/com/example/fizzbuzz/service/FizzBuzzService.java
@@ -6,7 +6,20 @@ import org.springframework.stereotype.Service;
 public class FizzBuzzService {
 
   public String[] calculateFizzBuzz(int input) {
-    String[] result = {"1", "2", "Fizz"};
+    String[] result = new String[input];
+
+    for (int i = 1; i <= input; i++) {
+      int index = i - 1;
+
+      if (i % 3 == 0) {
+        result[index] = "Fizz";
+      } else if (i % 5 == 0) {
+        result[index] = "Buzz";
+      } else {
+        result[index] = i + "";
+      }
+    }
+
     return result;
   }
 }
